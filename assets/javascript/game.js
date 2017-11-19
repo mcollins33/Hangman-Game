a46e6273d5dfcbdd5fb62be58b64a9f0807b463e
mcollins33 <collins.miki@gmail.com>
2017-11-19 16:37:09 -0500

    add style, wins, pictures, create animal objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,100 @@
+tiger = {
+	name: "tiger",
+	pic: "<img src='./assets/images/tiger.png' />",
 
+},
 
+giraffe = {
+	name: "giraffe",
+	pic: "<img src='./assets/images/giraffe.png' />",
+},
+
+zebra = {
+	name: "zebra",
+	pic: "<img src='./assets/images/zebra.png' />",
+},
+
+lion = {
+	name: "lion",
+	pic: "<img src='./assets/images/lion.png' />",
+},
+
+monkey = {
+	name: "monkey",
+	pic: "<img src='./assets/images/monkey.jpg' />",
+},
+
+turkey = {
+	name: "turkey",
+	pic: "<img src='./assets/images/turkey.jpg' />",
+},
+
+jaguar = {
+	name: "jaguar",
+	pic: "<img src='./assets/images/jaguar.gif' />",
+},
+
+bear = {
+	name: "bear",
+	pic: "<img src='./assets/images/bear.jpg' />",
+},
+
+squirrel = {
+	name: "squirrel",
+	pic: "<img src='./assets/images/squirrel.jpg' />",
+},
+
+deer = {
+	name: "deer",
+	pic: "<img src='./assets/images/deer.png' />",
+},
+
+bird = {
+	name: "bird",
+	pic: "<img src='./assets/images/bird.png' />",
+},
+
+snake = {
+	name: "snake",
+	pic: "<img src='./assets/images/snake.gif' />",
+},
+
+raccoon = {
+	name: "racoon",
+	pic: "<img src='./assets/images/raccoon.gif' />",
+},
+
+kangaroo = {
+	name: "kangaroo",
+	pic: "<img src='./assets/images/kangaroo.png' />",
+},
+
+moose = {
+	name: "moose",
+	pic: "<img src='./assets/images/moose.png' />",
+},
+
+fox = {
+	name: "fox",
+	pic: "<img src='./assets/images/fox.png' />",
+},
+
+leopard = {
+	name: "leopard",
+	pic: "<img src='./assets/images/leopard.png' />",
+},
+
+owl = {
+	name: "owl",
+	pic: "<img src='./assets/images/owl.jpg' />",
+},
+
+elephant = {
+	name: "elephant",
+	pic: "<img src='./assets/images/elephant.jpg' />",
+},
+
+rabbit = {
+	name: "rabbit",
+	pic: "<img src='./assets/images/rabbit.gif' />",
+}
